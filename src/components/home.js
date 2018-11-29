f0db9d679891b49b2bf0ad8040e73c0abd5ea686
devinaanderson <daa7896@gmail.com>
2018-11-29 14:04:15 -0700

    built card component and input function

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import  {Header}  from './header';
+import Card from './card';
+
 
 class Home extends Component {
   render() {
     return (
       <div className='home'>
         <Header title='Madlibs' content='Fill out the fields below and click the generate button to see the Mad Lib story!'/>
+        <Card/>
         
       </div>
     );
---
+++ src/components/input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Input = (props) => {
+    return (
+        <div>
+            <input/>
+            <label>{props.title}</label>        
+
+        </div>
+        
+    )
+}
+
+// export default Input
